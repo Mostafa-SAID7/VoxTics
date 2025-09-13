e26e37f6a2483ecf99acba98f2ae6793040087d3
Mostafa Said <m.ssaid356@gmail.com>
2025-09-14 00:03:44 +0300

    updates

--- VoxTics/Controllers/HomeController.cs
+++ VoxTics/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using VoxTics.Services.Implementations;
 using VoxTics.Services.Interfaces;
 
 namespace VoxTics.Controllers
@@ -16,25 +17,27 @@ namespace VoxTics.Controllers
         private readonly IHomeService _homeService;
         public HomeController(IHomeService homeService) => _homeService = homeService;
 
-        public async Task<IActionResult> Index()
+
+public async Task<IActionResult> Index()
+    {
+        var nowShowing = await _homeService.GetNowShowingAsync();
+        var comingSoon = await _homeService.GetComingSoonAsync();
+        var cinemas = await _homeService.GetCinemasAsync();
+        var featured = await _homeService.GetFeaturedMoviesAsync();
+
+        var viewModel = new HomeVM
         {
-            var nowShowing = await _homeService.GetNowShowingAsync();
-            var comingSoon = await _homeService.GetComingSoonAsync();
-            var cinemas = await _homeService.GetCinemasAsync();
-            var featured = await _homeService.GetFeaturedMoviesAsync();
+            NowShowing = nowShowing,
+            ComingSoon = comingSoon,
+            Cinemas = cinemas,
+            Featured = featured
+        };
 
-            var viewModel = new
-            {
-                NowShowing = nowShowing,
-                ComingSoon = comingSoon,
-                Cinemas = cinemas,
-                Featured = featured
-            };
+        return View(viewModel);
+    }
 
-            return View(viewModel);
-        }
 
-        public IActionResult Contact() => View();
+    public IActionResult Contact() => View();
         public IActionResult About() => View();
     }
 
--- VoxTics/Models/ViewModels/HomeVM.cs
+++ VoxTics/Models/ViewModels/HomeVM.cs
@@ -5,14 +5,9 @@ namespace VoxTics.Models.ViewModels
 {
     public class HomeVM
     {
-        public List<MovieVM> FeaturedMovies { get; set; } = new();
-        public List<MovieVM> NowShowingMovies { get; set; } = new();
-        public List<MovieVM> UpcomingMovies { get; set; } = new();
-        public List<CategoryVM> PopularCategories { get; set; } = new();
-        public List<ShowtimeVM> TodayShowtimes { get; set; } = new();
-
-        public int TotalMovies { get; set; }
-        public int TotalUpcoming { get; set; }
-        public int TotalNowShowing { get; set; }
+        public IEnumerable<Movie> NowShowing { get; set; }
+        public IEnumerable<Movie> ComingSoon { get; set; }
+        public IEnumerable<Cinema> Cinemas { get; set; }
+        public IEnumerable<Movie> Featured { get; set; }
     }
 }
