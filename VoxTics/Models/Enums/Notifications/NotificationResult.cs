cd98bdf24424c4d3936d94daebc29d1fc615362a
Mostafa Said <m.ssaid356@gmail.com>
2025-09-24 01:29:27 +0300

    finish admin category

---
+++ VoxTics/Models/Enums/Notifications/NotificationResult.cs
@@ -0,0 +1,11 @@
+﻿namespace VoxTics.Models.Enums.Notifications
+{
+    public class NotificationResult
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public string? NotificationId { get; set; }
+        public List<Notification>? Notifications { get; set; }
+        public object? Data { get; set; }
+    }
+}
